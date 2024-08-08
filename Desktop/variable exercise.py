75a4c4ea93f7df6371950f69c52c313217d0fd16
daviddiogo <david.a.r.diogo@gmail.com>
2024-08-08 21:46:15 +0200

    exercise 1

---
+++ Desktop/variable exercise.py
@@ -0,0 +1,10 @@
+nom_utilisateur = "David Diogo"
+est_abonne = True
+chaine_caractere = str(est_abonne)
+print("l'utilisateur" , nom_utilisateur , "est abonne : ", est_abonne)
+#il est parfois necessaire de changer le type de variable car certains types de donnees ne peuvent pas etre utiliser avec certaines operations sans une variable#
+print(type(est_abonne))
+print(chaine_caractere)
+message = "Est-ce que" , nom_utilisateur , "est abonne ?" , est_abonne , nom_utilisateur , "est abonne"
+print(message)
+#avec cette exercise j'ai appris a combiner les variable est a faire un push vers git hub
